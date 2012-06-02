8a685fcfc8b9a55bccee6e9d8e355da3f93d9016
Matthew Erbs <matthew.erbs@merbla.com>
2012-06-02 16:06:33 +1000

    Quick tests for json serialisation

--- src/KinectUs.Json/JsonExtensions.cs
+++ src/KinectUs.Json/JsonExtensions.cs
@@ -1,36 +1,63 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using KinectUs.Core.Structures;
 using Newtonsoft.Json;
-using JsonSerializer = ServiceStack.Text.JsonSerializer;
+
 
 namespace KinectUs.Json
 {
     public static class JsonExtensions
     {
-        public static string ToJson(this SkeletonFrameData skeleton)
-        {
-            var json = JsonSerializer.SerializeToString(skeleton);
-            return json;
-        }
+        //public static string ToJson(this SkeletonFrameData skeleton)
+        //{
+        //    var json = JsonSerializer.SerializeToString(skeleton);
+        //    return json;
+        //}
 
-        public static string ToJson(this Microsoft.Kinect.Skeleton skeleton)
-        {
-            var json = JsonSerializer.SerializeToString(skeleton);
-            return json;
-        }
+        //public static string ToJson(this Microsoft.Kinect.Skeleton skeleton)
+        //{
+        //    var json = JsonSerializer.SerializeToString(skeleton);
+        //    return json;
+        //}
 
         public  static string ToJson(this Microsoft.Kinect.Joint joint)
         {
-            //var json = JsonSerializer.SerializeToString()<Microsoft.Kinect.Joint>(joint);
 
-            var json = JsonConvert.SerializeObject(joint, Formatting.Indented);
+            var json = JsonConvert.SerializeObject(joint, Formatting.Indented, new KinectEnumConverter());
             return json;
         }
 
         public static string ToJson(this Microsoft.Kinect.JointCollection joints)
         {
-            var json = JsonConvert.SerializeObject(joints, Formatting.Indented);
+            var json = JsonConvert.SerializeObject(joints, Formatting.Indented, new KinectEnumConverter());
             return json;
         }
     }
+
+    public class KinectEnumConverter : Newtonsoft.Json.Converters.StringEnumConverter
+    {
+
+        public override bool CanConvert(Type objectType)
+        {
+            return (
+
+                       objectType == typeof (Microsoft.Kinect.JointTrackingState) ||
+                       objectType == typeof (Microsoft.Kinect.SkeletonTrackingState) ||
+                       objectType == typeof (Microsoft.Kinect.JointType)
+
+                   );
+        }
+
+
+        public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
+        {
+            if (value is Action)
+            {
+                writer.WriteValue(Enum.GetName(typeof(Action), (Action)value));// or something else
+                return;
+            }
+
+            base.WriteJson(writer, value, serializer);
+        }
+    } 
 }
---
+++ src/KinectUs.Tests/Json/KinectEnumConvertFixture.cs
@@ -0,0 +1,20 @@
+﻿using KinectUs.Json;
+using Microsoft.Kinect;
+using NUnit.Framework;
+
+namespace KinectUs.Tests.Json
+{
+    [TestFixture]
+    public class KinectEnumConvertFixture
+    {
+        [Test]
+        public void T()
+        {
+
+            var j = new Joint {TrackingState = JointTrackingState.Tracked};
+
+            var json = j.ToJson();
+            Assert.IsTrue(json.Contains("Tracked"));
+        }
+    }
+}
