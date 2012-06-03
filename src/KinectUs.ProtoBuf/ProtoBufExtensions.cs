a1c05712ec94189902ae289c35ceb8e5e1e151e2
Matthew Erbs <matthew.erbs@merbla.com>
2012-06-03 10:09:41 +1000

    Protobuf properties

--- src/KinectUs.ProtoBuf/ProtoBufExtensions.cs
+++ src/KinectUs.ProtoBuf/ProtoBufExtensions.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections.Generic;
 using System.IO;
+using Microsoft.Kinect;
 using ProtoBuf.Meta;
 
 namespace KinectUs.ProtoBuf
@@ -8,27 +9,44 @@ namespace KinectUs.ProtoBuf
     {
         private static RuntimeTypeModel _model;
 
-        public static Stream ToStream(this IEnumerable<Microsoft.Kinect.Skeleton> skeletons, Stream destination)
-        { 
-            Model.Serialize(destination, skeletons);
-            return destination;
-        }
+
+        private static readonly string[] _jointProperties = { 
+                                                                   "Position",   
+                                                                   "TrackingState",
+                                                                   "JointType"
+                                                               };
+        private static readonly string[] _skeletonProperties = {
+                                                                   "Joints",
+                                                                   "Position", 
+                                                                   "TrackingId", 
+                                                                   "TrackingState",
+                                                                   "ClippedEdges",
+                                                                   "BoneOrientations"
+                                                               };
 
         public static RuntimeTypeModel Model
         {
             get
             {
-                if(_model==null)
+                if (_model == null)
                 {
                     _model = TypeModel.Create();
-                    _model.Add(typeof(Microsoft.Kinect.Skeleton), false)
-                                 .Add("Joints", "Position", "TrackingId", "TrackingState", "ClippedEdges", "BoneOrientations");
-     
+                    _model.Add(typeof (Skeleton), false)
+                        .Add(_skeletonProperties);
+
+                    _model.Add(typeof (Joint), false)
+                        .Add(_jointProperties);
+
                 }
 
                 return _model;
             }
-        
+        }
+
+        public static Stream ToStream(this IEnumerable<Skeleton> skeletons, Stream destination)
+        {
+            Model.Serialize(destination, skeletons);
+            return destination;
         }
     }
 }
